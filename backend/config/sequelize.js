46e85d62e95b38228d48fd8d5653b59ce917b430
fer-jesus <limajesus909@gmail.com>
2025-06-06 00:18:07 -0600

    crud with ORM v2

--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -8,19 +8,9 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    //logging: false, // Puedes activar para debug
+    
   });
 
 module.exports = sequelize;
 
 
-// async function testConnection() {
-//   try {
-//     await sequelize.authenticate();
-//     console.log('Conexión a la base de datos establecida correctamente.');
-//   } catch (error) {
-//     console.error('No se pudo conectar a la base de datos:', error);
-//   }
-// }
-
-// testConnection();
