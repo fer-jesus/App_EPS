de798ebc214cf0f0dac003ddd88d73905667c1a8
fer-jesus <limajesus909@gmail.com>
2025-06-23 09:20:00 -0600

    solución para duplicar la misma tarifa

--- backend/models/tasas/tasaTarifa.model.js
+++ backend/models/tasas/tasaTarifa.model.js
@@ -1,31 +1,41 @@
 module.exports = (sequelize, DataTypes) => {
-  const TasaTarifa = sequelize.define("TasaTarifa", {
-    TASAS_id_tasa: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false
+  const TasaTarifa = sequelize.define(
+    "TasaTarifa",
+    {
+      tarifa_correlativo: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      TASAS_id_tasa: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+      },
+      TARIFA_id_nombreTarifa: {
+        type: DataTypes.INTEGER,
+        //primaryKey: true,
+        allowNull: false,
+      },
+      dimension_construccion: {
+        type: DataTypes.DECIMAL,
+        allowNull: true,
+      },
+      formula: {
+        type: DataTypes.STRING(250),
+        allowNull: false,
+      },
+      valor: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
     },
-    TARIFA_id_nombreTarifa: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false
-    },
-    dimension_construccion: {
-      type: DataTypes.DECIMAL,
-      allowNull: true
-    },
-    formula: {
-      type: DataTypes.STRING(250),
-      allowNull: false
-    },
-    valor: {
-      type: DataTypes.DECIMAL,
-      allowNull: false
+    {
+      tableName: "TASAS_TARIFA",
+      timestamps: false,
     }
-  }, {
-    tableName: "TASAS_TARIFA",
-    timestamps: false
-  });
+  );
 
   return TasaTarifa;
 };
